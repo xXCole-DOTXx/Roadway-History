c1cb44c9ed687f6e16f987cf2aa7472273526cd1
Cole Perry <E096752@wv.gov>
2021-03-03 15:58:26 -0500

    Housekeeping

--- Controllers/StatewidesController.cs
+++ Controllers/StatewidesController.cs
@@ -64,8 +64,6 @@ namespace Roadway_History.Controllers
         // GET: Statewides/Details/5
         public ActionResult Details(int? id)
         {
-            System.Diagnostics.Debug.WriteLine("********************** id being sent ******************************");
-            System.Diagnostics.Debug.WriteLine(id);
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
