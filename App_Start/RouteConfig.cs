aed7d9a24463b13d45a9ae0f1289d5fc065f1dc6
xXCole-DOTXx <cole.k.perry@wv.gov>
2021-05-24 12:28:42 -0400

    Changed landing page

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Roadway_History
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Statewides", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
